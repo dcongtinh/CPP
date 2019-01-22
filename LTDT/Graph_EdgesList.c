e95ce72ab1108d7b15ad318a1b1ddb696f02e60e
Tinh Dao <dcongtinh@gmail.com>
2019-01-22 09:06:03 +0700

    Add Graph_EdgesList.c

---
+++ LTDT/Graph_EdgesList.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#define MAX_M 500
+
+typedef struct {
+    int u, v;
+} Edge;
+
+
+typedef struct {
+    Edge edges[MAX_M];
+    int n, m;
+} Graph;
+
+void init_graph(Graph *pG, int n, int m)
+{
+    pG->n = n, pG->m = 0;
+}
+
+void print_graph(Graph *pG)
+{
+    int n = pG->n, m = pG->m;
+    printf("%d %d\n", n, m);
+    for (int i = 0; i < m; ++i)
+    {
+        printf("%d %d\n", pG->edges[i].u, pG->edges[i].v);
+    }
+}
+
+void add_edge(Graph *pG, int u, int v)
+{
+    int m = pG->m;
+    pG->edges[m].u = u;
+    pG->edges[m].v = v;
+    ++pG->m;
+}
+
+int degree(Graph *pG, int u)
+{
+    int cnt = 0;
+    for (int i = 0; i < pG->m; ++i)
+        cnt += (pG->edges[i].u == u || pG->edges[i].v == u);
+    return cnt;
+}
+
+int adjacent(Graph *pG, int u, int v) ///u adjacent v
+{
+    for (int i = 0; i < pG->m; ++i)
+    {
+        if ((pG->edges[i].u == u && pG->edges[i].v == v) || (pG->edges[i].v == u && pG->edges[i].u == v))
+            return 1;
+    }
+    return 0;
+}
+
+void neighbours(Graph *pG, int x)
+{
+    for (int i = 1; i <= pG->n; ++i)
+    {
+        if (adjacent(pG, x, i))
+            printf("%d ", i);
+    }
+}
+int main()
+{
+    Graph G;
+    int n, m;
+    scanf("%d%d", &n, &m);
+    init_graph(&G, n, m);
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        add_edge(&G, u, v);
+    }
+
+    print_graph(&G);
+}
