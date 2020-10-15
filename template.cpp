d3d7cbc7d1d812895a45d5622a028b8a4d53cf8b
Tinh Dao <dcongtinh@gmail.com>
2020-10-15 17:21:32 +0700

    .

--- template.cpp
+++ template.cpp
@@ -1,34 +1,8 @@
-#include <stdio.h>
-#include <iostream>
+#include <bits/stdc++.h>
 
-#include "\\vmware-host\Shared Folders\Desktop\C++\lib\bstlib.h"
 using namespace std;
 
-int main(){
-	FILE *fi, *fo;
-
-    fi = fopen("input.txt", "r");
-    fo = fopen("output.txt", "w");
-    int n, a[10];
-    fscanf(fi, "%d", &n);
-
-    for (int i = 1; i <= n; ++i) fscanf(fi, "%d", &a[i]);
-//    fclose(fi);
-    for (int i = 1; i <= n; ++i) fprintf(fo, "%d", a[i]);
-	return 0;
+int main()
+{
+    printf("abc");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
